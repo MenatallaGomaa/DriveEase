6e72b3ae349696555d3171e89f558ad7177b0247
Menatalla Gomaa <minafathi98@gmail.com>
2024-02-15 17:12:25 +0100

    About section DONE

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import About from "./components/About/About";
 
 const App = () => {
   // dark mode start
@@ -39,6 +40,7 @@ const App = () => {
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero theme={theme} />
+      <About />
     </div>
   );
 };
